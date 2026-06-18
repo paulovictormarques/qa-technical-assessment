f664ee87ef9b272b10d95446b2174f9f69799063
Paulo Victor Marques <paulovictor720@gmail.com>
2026-06-18 12:18:05 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // Se seus .cy.js estão no root, permita esse padrão; ideal é mover os testes para cypress/e2e
-    specPattern: '*.cy.js',
+    specPattern: 'cypress/e2e/*.cy.js',
     // desative supportFile se você não usa um
     supportFile: false,
     // opcional: baseUrl: 'https://seu-site-de-teste.local'
